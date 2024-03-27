25bd362c69171253fe0e39e5a75d8015477858c7
XasanboyKarimov <xkarimov257@gmail.com>
2024-03-27 12:18:29 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,121 @@
+//4 masala
+// let a = -2;
+// let b = -12;
+// let c = 18;
+
+// let musbatSonlar = 0;
+// let manfiySonlar = 0;
+
+// if (a > 0) {
+//   musbatSonlar++;
+// } else {
+//   manfiySonlar++;
+// }
+
+// if (b > 0) {
+//   musbatSonlar++;
+// } else {
+//   manfiySonlar++;
+// }
+
+
+// if (c > 0) {
+//   musbatSonlar++;
+// } else {
+//   manfiySonlar++;
+// }
+
+// console.log("musbatSonlar", musbatSonlar);
+// console.log("manfiySonlar", manfiySonlar);
+
+//5 masala
+// let a = 2;
+// let b = 4;
+// let d = -20;
+// let c = 20;
+
+// let butunSonlar = 0; 
+// let togSonlar = 0;
+
+// let musbatSonlar = 0;
+// let manfiySonlar = 0;
+
+// if (a > 0) {
+//   musbatSonlar++;
+// } else {
+//   manfiySonlar++;
+// }
+
+// if (b > 0) {
+//   musbatSonlar++;
+// } else {
+//   manfiySonlar++;
+// }
+
+
+// if (c > 0) {
+//   musbatSonlar++;
+// } else {
+//   manfiySonlar++;
+// }
+
+// if(d > 0){
+//   musbatSonlar++;
+// } else{
+//   manfiySonlar++;
+// }
+
+// console.log("musbatSonlar", musbatSonlar);
+// console.log("manfiySonlar", manfiySonlar);
+
+//6 masala
+// let a = 6;
+// let b = 3; 
+
+// if(a != b){
+//   console.log("A katta son");
+// } else{
+//   console.log("B katta son");
+// }
+
+//7 masala
+// let katta = 3;
+// let kichkina = 1;
+
+// let kattaSon = 0;
+// let kichkinaSon =0;
+
+// if(kichkina == katta){
+//   kattaSon
+// } else{
+//   kichkinaSon
+// }
+
+//9 masala
+// let  a = 3;
+// let  b = 4;
+
+// if(a + b){
+//     console.log( a + b);
+// } else{
+//     console.log("boshqa sonb kiriting");
+// }
+
+//10 masala
+// let  a = 3;
+// let  b = 4;
+
+// if(a + b){
+//     console.log( a + b);
+// } else{
+//     console.log("boshqa sonb kiriting");
+// }
+
+//13 masala
+
+let a = 5;
+let b = 8;
+
+if(a > b){
+    console.log(a > b);
+}
